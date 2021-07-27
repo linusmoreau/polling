c2edda6b8fb57d33708eeada934352a085db1358
Linus Moreau <linus.moreau@gmail.com>
2021-07-26 20:02:57 -0700

    Add display caption and icon for window

--- polling.py
+++ polling.py
@@ -1852,4 +1852,11 @@ if __name__ == '__main__':
     menu_page = MenuPage(order)
     surface = pygame.Surface((screen_width, screen_height))
     surface.fill(whitish)
+    pygame.display.set_caption('Polling')
+
+    icon = pygame.transform.scale((pygame.image.load("images/graph.png")), (32, 32))
+    icon_surf = pygame.Surface((32, 32))
+    icon_surf.fill(white)
+    icon_surf.blit(icon, (0, 0), None)
+    pygame.display.set_icon(icon_surf)
     run_loop(lock, background=surface)
